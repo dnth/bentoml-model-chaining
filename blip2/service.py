f67f9b05c535c38dd787ef18a813df807c31bd61
dnth <dickson.neoh@gmail.com>
2024-08-21 11:45:59 +0800

    inital update

---
+++ blip2/service.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+import bentoml
+
+
+from pathlib import Path
+import typing as t
+from bentoml.validators import ContentType
+from PIL import Image as PILImage
+
+
+Image = t.Annotated[Path, ContentType("image/*")]
+
+@bentoml.service(resources={"gpu": 1})
+class Captioning:
+    def __init__(self) -> None:
+        import torch
+        from transformers import pipeline
+
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.pipeline = pipeline("image-to-text", model="Salesforce/blip2-opt-2.7b", device=device)
+
+    @bentoml.api(batchable=True)
+    def caption(self, image: list[Image]) -> list[str]:
+        images = [PILImage.open(img) for img in image]
+        results = self.pipeline(images)
+        print(results)
+        return [item["generated_text"] for sublist in results for item in sublist]
