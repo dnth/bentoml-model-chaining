fd4fa2b725377130c848e4994e65feb76d1b82c0
dnth <dickson.neoh@gmail.com>
2024-08-21 17:08:34 +0800

    add logger

--- yolo_blip2_parallel/service.py
+++ yolo_blip2_parallel/service.py
@@ -2,29 +2,27 @@ from __future__ import annotations
 
 import asyncio
 import json
-import os
+from loguru import logger
 import typing as t
 from pathlib import Path
 
 import bentoml
 from bentoml.validators import ContentType
 from PIL import Image as PILImage
+import time
 
 Image = t.Annotated[Path, ContentType("image/*")]
 
 
 @bentoml.service()
-class YoloV8:
+class ObjectDetector:
     def __init__(self):
         import torch
         from ultralytics import YOLO
 
-        yolo_model = os.getenv("YOLO_MODEL", "yolov8x.pt")
         device = "cuda" if torch.cuda.is_available() else "cpu"
-        print("#####")
-        print(f"Using device: {device}")
-        print("#####")
-        self.model = YOLO(yolo_model).to(device=device)
+        logger.info(f"Object detector using device: {device}")
+        self.model = YOLO("yolov8x.pt").to(device=device)
 
     @bentoml.api(batchable=True)
     async def predict(self, images: list[Image]) -> list[list[dict]]:
@@ -43,44 +41,38 @@ class YoloV8:
     
 
 @bentoml.service()
-class Captioning:
+class Captioner:
     def __init__(self) -> None:
         import torch
         from transformers import pipeline
 
         device = "cuda" if torch.cuda.is_available() else "cpu"
-        # print("#####")
-        # print(f"Using device: {device}")
-        # print("#####")
+        logger.info(f"Captioner using device: {device}")
         self.pipeline = pipeline("image-to-text", model="Salesforce/blip2-opt-2.7b", device=device)
 
     @bentoml.api(batchable=True)
     async def caption(self, image: list[Image]) -> list[str]:
         images = [PILImage.open(img) for img in image]
         results = self.pipeline(images)
-        # print("#####")
-        # print(results)
-        # print("#####")
         return [item["generated_text"] for sublist in results for item in sublist]
 
 
 @bentoml.service(resources={"gpu": 1})
 class ParallelBLIP2YOLO:
-    object_detection = bentoml.depends(YoloV8)
-    captioning = bentoml.depends(Captioning)
+    object_detection = bentoml.depends(ObjectDetector)
+    captioning = bentoml.depends(Captioner)
 
-    # @bentoml.api()
-    # def sequential_inference(self, images: list[Image]) -> t.Tuple[list[list[dict]], list[str]]:
-    #     object_detection = self.object_detection.predict(images)
-    #     caption = self.captioning.caption(images)
-
-    #     return object_detection, caption
-    
     @bentoml.api()
-    async def parallel_inference(self, images: list[Image]) -> t.Tuple[list[list[dict]], list[str]]:
+    async def parallel_inference(self, images: list[Image]) -> t.Tuple[list[list[dict]], list[str], float]:
+        start_time = time.time()
+
         object_detection, caption = await asyncio.gather(
             self.object_detection.predict(images),
             self.captioning.caption(images),
         )
 
-        return object_detection, caption
+        end_time = time.time()
+        runtime = end_time - start_time
+        logger.info(f"Parallel inference took {runtime:.2f} seconds")
+
+        return object_detection, caption, runtime
