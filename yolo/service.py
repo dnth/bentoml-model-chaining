8327607580c285f6e73fe694a8c81d480aa0ba08
dnth <dickson.neoh@gmail.com>
2024-08-29 12:57:00 +0800

    update yolo deployment to bentocloud

--- yolo/service.py
+++ yolo/service.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import json
-import os
 import typing as t
 from pathlib import Path
 
@@ -15,22 +14,30 @@ Image = t.Annotated[Path, ContentType("image/*")]
 class YoloV8:
     def __init__(self):
         from ultralytics import YOLO
+
         self.model = YOLO("yolov8s.pt")
 
     @bentoml.api(batchable=True)
-    def predict(self, images: list[Image]) -> list[list[dict]]:
+    def detect_files(self, images: list[Image]) -> list[list[dict]]:
         results = self.model.predict(source=images)
         return [json.loads(result.tojson()) for result in results]
 
+    @bentoml.api(batchable=True)
+    def detect_urls(self, urls: list[str]) -> list[list[dict]]:
+        results = self.model.predict(source=urls)
+        return [json.loads(result.tojson()) for result in results]
+
     @bentoml.api
-    def render(self, image: Image) -> Image:
+    def render_detect_file(self, image: Image) -> Image:
         result = self.model.predict(image)[0]
         output = image.parent.joinpath(f"{image.stem}_result{image.suffix}")
         result.save(str(output))
         return output
-    
+
     @bentoml.api
-    def detect_objects_file(self, image: Image) -> list[dict]:
-        result = self.model.predict(image)[0]
-        print(result)
-        return json.loads(result.tojson())
+    def render_detect_url(self, url: str) -> Image:
+        result = self.model.predict(url)[0]
+        output_filename = f"result_{hash(url)}.jpg"
+        output = Path(output_filename)
+        result.save(str(output))
+        return output
