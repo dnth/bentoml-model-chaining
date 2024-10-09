f7436556b68e8ac2228077ca0eaeac2b28b4832d
dnth <dickson.neoh@gmail.com>
2024-10-09 16:15:07 +0800

    update phi35

--- phi3.5-vision/service.py
+++ phi3.5-vision/service.py
@@ -1,131 +1,80 @@
 from __future__ import annotations
-import bentoml
-from pathlib import Path
+
 import typing as t
+from pathlib import Path
+
+import bentoml
+import requests
+import torch
 from bentoml.validators import ContentType
 from PIL import Image as PILImage
-import requests
 from transformers import AutoModelForCausalLM, AutoProcessor
 
 Image = t.Annotated[Path, ContentType("image/*")]
 
-@bentoml.service(resources={"gpu": 1})
+
+@bentoml.service()
 class Phi35VisionService:
     def __init__(self) -> None:
-        model_id = "microsoft/Phi-3.5-vision-instruct"
+        model_id = "./models-phi-35-vision"
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
         self.model = AutoModelForCausalLM.from_pretrained(
             model_id,
-            device_map="cuda",
+            device_map=self.device,
             trust_remote_code=True,
-            torch_dtype="auto",
-            _attn_implementation='flash_attention_2'
+            torch_dtype=torch.bfloat16,
+            _attn_implementation="flash_attention_2",
         )
         self.processor = AutoProcessor.from_pretrained(
-            model_id,
-            trust_remote_code=True,
-            num_crops=16
+            model_id, trust_remote_code=True, num_crops=16
         )
 
-    def _process_images(self, images: list[Image | str]) -> list[PILImage.Image]:
-        pil_images = []
-        for img in images:
-            if isinstance(img, str):
-                pil_images.append(PILImage.open(requests.get(img, stream=True).raw))
-            else:
-                pil_images.append(PILImage.open(img))
-        return pil_images
+        self.model = torch.compile(self.model, mode="max-autotune")
 
-    def _generate_response(self, images: list[PILImage.Image], prompt: str) -> str:
-        placeholder = "".join([f"<|image_{i+1}|>\n" for i in range(len(images))])
+    def _run_inference(
+        self, image: PILImage.Image, prompt_text: str, max_new_tokens: int = 50
+    ) -> str:
+        # Note: Batch inference is not supported for this model
+        placeholder = f"<|image_1|>\n"
         messages = [
-            {"role": "user", "content": placeholder + prompt},
+            {"role": "user", "content": placeholder + prompt_text},
         ]
-        prompt = self.processor.tokenizer.apply_chat_template(
-            messages,
-            tokenize=False,
-            add_generation_prompt=True
+        prompt_text = self.processor.tokenizer.apply_chat_template(
+            messages, tokenize=False, add_generation_prompt=True
         )
-        inputs = self.processor(prompt, images, return_tensors="pt").to("cuda:0")
-        generation_args = {
-            "max_new_tokens": 1000,
-            "temperature": 0.0,
-            "do_sample": False,
-        }
-        generate_ids = self.model.generate(
-            **inputs,
-            eos_token_id=self.processor.tokenizer.eos_token_id,
-            **generation_args
-        )
-        generate_ids = generate_ids[:, inputs['input_ids'].shape[1]:]
-        return self.processor.batch_decode(
-            generate_ids,
-            skip_special_tokens=True,
-            clean_up_tokenization_spaces=False
-        )[0]
 
-    # @bentoml.api(batchable=True)
-    # def analyze(self, images: list[Image | str], prompt: str) -> str:
-    #     pil_images = self._process_images(images)
-    #     return self._generate_response(pil_images, prompt)
-
-    # @bentoml.api()
-    # def ocr(self, image: Image) -> str:
-    #     return self.analyze([image], "Perform OCR on this image and extract all the text you can see.")
-
-    # @bentoml.api()
-    # def chart_analysis(self, image: Image) -> str:
-    #     return self.analyze([image], "Analyze this chart or table. Describe its type, main components, and key insights.")
-
-    # @bentoml.api()
-    # def compare_images(self, images: list[Image]) -> str:
-    #     return self.analyze(images, "Compare these images and describe the similarities and differences.")
-
-    # @bentoml.api()
-    # def summarize_sequence(self, images: list[Image]) -> str:
-        return self.analyze(images, "Summarize the sequence of images as if they were frames from a video clip.")
-    
-    @bentoml.api()
-    def caption_image_file(self, image: Image) -> str:
-        prompt = "Describe the image in detail, focusing on the main subjects, their actions, and the overall setting. Include information about colors, textures, and any notable objects or elements in the background."
-        pil_image = self._process_images([image])[0]
-        return self._generate_response([pil_image], prompt)
-    
-    @bentoml.api()
-    def caption_image_url(self, image_url: str) -> str:
-        prompt = "Describe the image in detail, focusing on the main subjects, their actions, and the overall setting. Include information about colors, textures, and any notable objects or elements in the background."
-        pil_image = self._process_images([image_url])[0]
-        return self._generate_response([pil_image], prompt)
+        with torch.inference_mode(), torch.cuda.amp.autocast(dtype=torch.bfloat16):
+            inputs = self.processor(prompt_text, image, return_tensors="pt").to(
+                self.device
+            )
+            generate_ids = self.model.generate(
+                **inputs,
+                max_new_tokens=max_new_tokens,
+                do_sample=False,
+                temperature=0.0,
+                eos_token_id=self.processor.tokenizer.eos_token_id,
+            )
+            generate_ids = generate_ids[:, inputs["input_ids"].shape[1] :]
+            return self.processor.batch_decode(
+                generate_ids,
+                skip_special_tokens=True,
+                clean_up_tokenization_spaces=False,
+            )[0]
 
     @bentoml.api()
-    def ocr_image_file(self, image: Image) -> str:
-        prompt = "Perform OCR on this image and extract all the text you can see."
-        pil_image = self._process_images([image])[0]
-        return self._generate_response([pil_image], prompt)
-    
-    @bentoml.api()
-    def ocr_image_url(self, image_url: str) -> str:
-        prompt = "Perform OCR on this image and extract all the text you can see."
-        pil_image = self._process_images([image_url])[0]
-        return self._generate_response([pil_image], prompt)
+    def vqa_image_url(
+        self, image_url: str, question: str, max_new_tokens: int = 50
+    ) -> str:
+        image = PILImage.open(requests.get(image_url, stream=True).raw)
+        return self._run_inference(image, question, max_new_tokens)
 
     @bentoml.api()
-    def chart_analysis_image_file(self, image: Image) -> str:
-        prompt = "Analyze this chart or table. Describe its type, main components, and key insights."
-        pil_image = self._process_images([image])[0]
-        return self._generate_response([pil_image], prompt)
-    
-    @bentoml.api()
-    def chart_analysis_image_url(self, image_url: str) -> str:
-        prompt = "Analyze this chart or table. Describe its type, main components, and key insights."
-        pil_image = self._process_images([image_url])[0]
-        return self._generate_response([pil_image], prompt)
-    
-    @bentoml.api()
-    def analyze_image_file(self, image: Image, prompt: str) -> str:
-        pil_image = self._process_images([image])[0]
-        return self._generate_response([pil_image], prompt)
-    
-    @bentoml.api()
-    def analyze_image_url(self, image_url: str, prompt: str) -> str:
-        pil_image = self._process_images([image_url])[0]
-        return self._generate_response([pil_image], prompt)
+    def caption_image_url(self, image_url: str, max_new_tokens: int = 50) -> str:
+        image = PILImage.open(requests.get(image_url, stream=True).raw)
+        prompt = "Describe the image in concise, focusing on the main subjects, \
+                  their actions, and the overall setting. Include information about \
+                  colors, textures, and any notable objects or elements in the background. \
+                  Eliminate filler words, adverbs, and any unnecessary phrases, focusing solely \
+                  on the core meaning and essential information."
+        return self._run_inference(image, prompt, max_new_tokens)
