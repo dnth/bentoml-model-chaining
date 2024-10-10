2d08bd38185f18089811cc63ba815a716e69c0e2
dnth <dickson.neoh@gmail.com>
2024-10-11 00:11:53 +0800

    update moondream

--- moondream/service.py
+++ moondream/service.py
@@ -7,6 +7,7 @@ import bentoml
 import requests
 import torch
 from bentoml.validators import ContentType
+from loguru import logger
 from PIL import Image as PILImage
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
@@ -20,20 +21,23 @@ class MoondreamService:
         revision = "2024-08-26"
 
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        logger.info(f"Using device: {self.device}")
 
+        logger.info(f"Loading model from {model_id} with revision {revision}")
         self.model = AutoModelForCausalLM.from_pretrained(
             model_id,
             trust_remote_code=True,
             revision=revision,
             torch_dtype=torch.bfloat16,
             attn_implementation="flash_attention_2",
+            cache_dir="./moondream2",
         ).to(self.device)
 
         self.model = torch.compile(self.model, mode="max-autotune")
-
         self.tokenizer = AutoTokenizer.from_pretrained(model_id, revision=revision)
 
     def _run_inference(self, image: PILImage.Image, question: str) -> str:
+        logger.info(f"Running inference on image with question: {question}")
         with torch.inference_mode(), torch.cuda.amp.autocast(dtype=torch.bfloat16):
             enc_image = self.model.encode_image(image).to(self.device)
             return self.model.answer_question(enc_image, question, self.tokenizer)
